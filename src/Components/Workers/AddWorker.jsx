1a58966b6b67a3db5eab7e7ba85a45ba6af3b9ca
Jainam Shah <jainams6160@gmail.com>
2022-03-27 00:42:22 +0530

    Added Worker

--- src/Components/Workers/AddWorker.jsx
+++ src/Components/Workers/AddWorker.jsx
@@ -2,8 +2,27 @@ import React from 'react'
 
 const AddWorker = () => {
   return (
-    <div className='container'>
-        
+    <div className='container' style={{backgroundColor:"white", marginLeft:"300px", width:"1200px", marginTop:"20px", borderRadius:"20px"}}>
+        <div>
+            <form >
+              <div className='row' >
+                <div className='col-6 p-3 my-3' style={{zIndex:"2"}}><input type="text" className="form-control mx-4" placeholder='Full Name' style={{borderRadius:"20px", width:"500px"}}/></div>
+                <div className='col-6 p-3 my-3' style={{zIndex:"2"}}><input type="text" className="form-control mx-4" placeholder='Age' style={{borderRadius:"20px", width:"500px"}}/></div>
+              </div>
+              <div className='row'>
+                <div className='col-6 p-3 my-3' style={{zIndex:"2"}}><input type="text" className="form-control mx-4" placeholder='Aadhar Number' style={{borderRadius:"20px", width:"500px"}}/></div>
+                <div className='col-6 p-3 my-3' style={{zIndex:"2"}}><input type="text" className="form-control mx-4" placeholder='Work Location' style={{borderRadius:"20px", width:"500px"}}/></div>
+              </div>
+              <div className='row'>
+                <div className='col-6 p-3 my-3' style={{zIndex:"2"}}><input type="email" className="form-control mx-4" placeholder='Email' style={{borderRadius:"20px", width:"500px"}}/></div>
+                <div className='col-6 p-3 my-3' style={{zIndex:"2"}}><input type="text" className="form-control mx-4" placeholder='Address' style={{borderRadius:"20px", width:"500px"}}/></div>
+              </div>
+              <div className='row'>
+                <div className='col-6 p-3 my-3' style={{zIndex:"2"}}></div>
+                <div className='col-6 p-3 my-3' style={{zIndex:"2"}}></div>
+              </div>
+            </form>
+        </div>
     </div>
   )
 }
