f969681585473e1aaa36e2aee25286709cf09539
Jainam Shah <jainams6160@gmail.com>
2022-03-27 05:10:05 +0530

    Bug fixes

--- src/Components/Dashboard/StatsCard3.jsx
+++ src/Components/Dashboard/StatsCard3.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 
 const StatsCard3 = (props) => {
+  let data01=10
+  let data02 = 20
   return (
     <div>
         <div className="card col-md-4" style={{margin : "250px 0px 0 600px", borderRadius:"15px", position:"absolute"}}>
             <div className="card-body">
                 <h1 className="card-title" style={{fontSize:"30px"}}>Projects</h1>
                 <p className="card-text" style={{color:"black"}}>
-                    Attendance Reports
                 </p>
             </div>
         </div>
