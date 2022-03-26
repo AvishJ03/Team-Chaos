e38f067f54d1f4aeb1a8543def797aa239da06c0
Jainam Shah <jainams6160@gmail.com>
2022-03-26 14:48:33 +0530

    Started Routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Landing from "./Components/Landing/Landing";
+import Dashboard from "../src/Components/Dashboard/Dashboard"
 
 function App() {
   return(
-    <Landing/>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter> 
   );
 }
 
