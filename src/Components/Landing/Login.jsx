cd4a2810941520e730fc3ecd277eacc0d2e89cab
Jainam Shah <jainams6160@gmail.com>
2022-03-26 13:25:07 +0530

    Login Fix

--- src/Components/Landing/Login.jsx
+++ src/Components/Landing/Login.jsx
@@ -12,12 +12,12 @@ const Login = () => {
     <div>
         <div className="card px-3 rounded-me" style={{width: "25rem", margin:"150px 900px", textAlign:"center"}}>
             <div className="card-body ">
-                <h5 className="card-title pt-4" style={{fontSize:"30px"}}>{register || "Welcome Back..."}{register && "Hey There.."}</h5>
-                <h6 className="card-subtitle mb-2 text-muted">{register || "Please enter your email and password"}{register && "Please enter your Details"}</h6>
+                <h5 className="card-title pt-4" style={{fontSize:"30px"}}>{register && "Welcome Back..."}{register || "Hey There.."}</h5>
+                <h6 className="card-subtitle mb-2 text-muted">{register && "Please enter your email and password"}{register || "Please enter your Details"}</h6>
                 <Form state={register}/>
                 <div class="m-2">
-                {register || <p>New Here? <span className = "hover-effect" style = {{textDecoration:"underline", cursor: "pointer"}} onClick={changeRegister}>Register</span> </p>}
-                {register && <p>Already Signed Up? <span className = "hover-effect" style = {{textDecoration:"underline", cursor: "pointer"}} onClick={changeRegister}>Login</span> </p>}
+                {register && <p>New Here? <span className = "hover-effect" style = {{textDecoration:"underline", cursor: "pointer"}} onClick={changeRegister}>Register</span> </p>}
+                {register || <p>Already Signed Up? <span className = "hover-effect" style = {{textDecoration:"underline", cursor: "pointer"}} onClick={changeRegister}>Login</span> </p>}
                 </div>
             </div>
         </div>
