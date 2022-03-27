cf34be2ba02ae8fe69fe08e083a53f06769d3834
Jainam Shah <jainams6160@gmail.com>
2022-03-27 05:44:08 +0530

    add

--- src/Components/Workers/Row.jsx
+++ src/Components/Workers/Row.jsx
@@ -8,6 +8,7 @@ const Row = (props) => {
         <td className='py-3'>{props.name}</td>
         <td className='py-3'>{props.projectID}</td>
         <td className='py-3'>{props.attendance}</td>
+        <td style={{zIndex:"2"}}><button type="button" class="btn btn-light btn-lg px-4 mx-4" onClick={props.del} style={{borderRadius:"20px", background: "#3A477C", border: "none", color:"white"}}>Delete</button></td>
         </tr>
     </>
   )
--- src/Components/Workers/Table.jsx
+++ src/Components/Workers/Table.jsx
@@ -59,14 +59,15 @@ const Table = () => {
   }
 
   return (
-  <div className='px-3 container' style={{backgroundColor:"white", marginLeft:"300px", width:"1200px", marginTop:"15px", borderRadius:"20px"}}>
-    <table className="table">
+  <div className='px-3 container' style={{backgroundColor:"white", marginLeft:"300px", width:"1200px", marginTop:"15px", borderRadius:"20px", zIndex:"2"}}>
+    <table className="table" >
       <thead>
         <tr className = "mx-3" style={{height:"12px"}}>
           <th className = "py-3" scope="col" style={{color:"#A3A2A2"}}>Worker ID</th>
           <th className = "py-3" scope="col" style={{color:"#A3A2A2"}}>Name</th>
           <th className = "py-3" scope="col" style={{color:"#A3A2A2"}}>Project ID</th>
           <th className = "py-3" scope="col" style={{color:"#A3A2A2"}}>Attendance</th>
+          <th className = "py-3" scope="col" style={{color:"#A3A2A2"}}>Delete</th>
         </tr>
       </thead>
       <tbody> 
