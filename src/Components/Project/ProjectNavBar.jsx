2dcbc5209d3a0d1d270ceb240af9e337bf8b21e1
Jainam Shah <jainams6160@gmail.com>
2022-03-27 10:49:21 +0530

    show in maps

--- src/Components/Project/ProjectNavBar.jsx
+++ src/Components/Project/ProjectNavBar.jsx
@@ -15,14 +15,10 @@ const ProjectNavBar = (props) => {
         <div className='container-fluid' style={{margin:"0px", display:"inline"}}>
             <button type="button" class="btn btn-light btn-lg px-4 mx-4" onClick={props.add} style={{borderRadius:"20px", background: "#3A477C", border: "none", color:"white"}}>Add</button>
             <button type="button" class="btn btn-dark btn-lg px-4" style={{marginLeft:"920px", borderRadius:"20px", background: "#5A72D3", border: "none", color:"white"}} onClick={props.view} >View Details</button>
-            
             <ul>
-            <li style={{backgroundColor:"red"}}>
+            <li style={{background:"#3A477C", padding:"10px", width:"175px", textAlign:"center", borderRadius:"20px", marginLeft:"450px", color:"white", fontSize:"18px"}}>
                 <Link to="/gmap">
-                  <span class="icon">
-                    <ion-icon name="grid-outline"></ion-icon>
-                  </span>
-                  &nbsp;&nbsp;&nbsp;Dashboard
+                  Show in Maps
                 </Link>
               </li>
               </ul>
